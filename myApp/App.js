9cfce593f652f24495ca2bd90533d8aa485de9af
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2023-03-28 21:02:13 -0300

    starting state with sqliteStorage

--- myApp/App.js
+++ myApp/App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import Footer from "./layout/Footer";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import Header from "./layout/Header";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function plus() {
+    setCount(count + 1);
+  }
+
+  function less() {
+    setCount(count - 1);
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        <Text>Conteudo</Text>
+        <AntDesign name="pluscircle" size={24} color="black" onPress={plus} />
+        <MaterialIcons
+          name="do-not-disturb-on"
+          size={28}
+          color="black"
+          onPress={less}
+        />
+        <Text>{count}</Text>
       </View>
       <Footer />
     </View>
@@ -24,5 +43,6 @@ const styles = StyleSheet.create({
   },
   content: {
     height: "85%",
+    alignItems: "center",
   },
 });
